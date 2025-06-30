5a6ff3edfbd2466abdba2ed5889e9c113dc719da
Dulaj Hansana <dulajhansana1973@gmail.com>
2025-06-30 09:07:30 +0000

    use all text style as popins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,9 +17,9 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
-        code: ['"Source Code Pro"', 'monospace'],
+        body: ['Poppins', 'sans-serif'],
+        headline: ['Poppins', 'sans-serif'],
+        code: ['Poppins', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
